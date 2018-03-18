b00eb3e0b685591a536b2151ad43a4d3903fcda2
Ali Spittel <ali07cat07@gmail.com>
2018-03-18 18:56:20 -0400

    vue rewrite

---
+++ src/components/RainbowLetter.vue
@@ -0,0 +1,67 @@
+<template>
+  <span :class="{ hovered }" v-on:mouseover="hoverAnimation">{{ letter }}</span>
+</template>
+
+<script>
+  export default {
+    props: ['letter'],
+    data () {
+      return { hovered: false }
+    },
+    methods: {
+      toggleHover () {
+        this.hovered = !this.hovered
+      },
+      hoverAnimation () {
+        if (this.hovered) return
+        this.toggleHover()
+        setTimeout(this.toggleHover, 3200)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">  
+  span {
+    font-weight: 800;
+    letter-spacing: -20px;
+    mix-blend-mode: darken;
+    position: relative;
+    opacity: 0.8;
+    font-size: 20px;
+    text-transform: uppercase;
+
+    @media (max-width: 650px) {
+      letter-spacing: -10px;
+    }
+  }
+
+  .hovered {
+    -webkit-animation: hoveranimation ease 3s;
+    animation: hoveranimation ease 3s;
+  }
+
+  @keyframes hoveranimation {
+    0% {
+      top: 0px;
+    }
+    50% {
+      top: 50px;
+    }
+    100% {
+      top: 0px;
+    }
+  }
+
+  @-webkit-keyframes hoveranimation {
+    0% {
+      top: 0px;
+    }
+    50% {
+      top: 50px;
+    }
+    100% {
+      top: 0px;
+    }
+  }
+</style>
---
+++ src/router.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Projects from './views/Projects.vue'
+import Talks from './views/Talks.vue'
+import Contact from './views/Contact.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: Projects
+    },
+    {
+      path: '/talks',
+      name: 'talks',
+      component: Talks
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    }
+  ]
+})
